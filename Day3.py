c73739f1ecf09e736f44250fd3960fa5674acb3d
pavan3669 <52204850+pavan3669@users.noreply.github.com>
2019-08-08 14:00:03 +0530

    Add files via upload

---
+++ Day3.py
@@ -0,0 +1,123 @@
+
+# coding: utf-8
+
+# In[1]:
+
+import numpy as np
+
+
+# In[2]:
+
+array_nd = np.arange(12).reshape(3,4)
+
+array_nd
+
+
+# In[ ]:
+
+# Printing arrays
+
+# One dimensional array - printed as rows
+
+# Two dimensional array - printed as matrices
+
+# Tri dimensional array - as list of matrices
+
+
+# In[3]:
+
+# 1D array
+
+a = np.arange(6)
+
+print(a)
+
+
+# In[4]:
+
+# 2D array
+
+b = np.arange(12).reshape(4,3)
+
+print(b)
+
+
+# In[6]:
+
+# 3D array
+
+c = np.arange(24).reshape(2,3,4)
+
+c
+
+
+# In[10]:
+
+print(np.arange(10000))
+
+
+# In[11]:
+
+print(np.arange(3,10000))
+
+
+# In[12]:
+
+np.set_printoptions(threshold = np.nan)
+
+
+# In[13]:
+
+# Basic operations
+
+import numpy as np
+
+
+# In[16]:
+
+a = np.array([10,10,10])
+
+b = np.array([5,5,5])
+
+a+b
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
